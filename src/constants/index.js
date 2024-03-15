5b194b0952125406a6b002597240f2437c299bb0
Khizer Javed Iqbal <khizerjaved25@gmail.com>
2024-03-15 08:15:19 +0500

    deploying

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,5 @@
 export const TOKEN = "TOKEN";
 export const LOGGED_IN_USER = "loggedInUser";
-
 export const TOKEN_TYPE = "Bearer ";
 export const REQUEST_HEADER_AUTH_KEY = "Authorization";
 export const SERVER_API = process.env.REACT_APP_SERVER_API
