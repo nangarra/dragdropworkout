7c2d34d092d8adc2a7b2412c2bd98407cbb70590
Khizer Javed Iqbal <khizerjaved25@gmail.com>
2024-04-09 17:54:03 +0500

    done

--- src/layouts/workout-builder/list/exercises.js
+++ src/layouts/workout-builder/list/exercises.js
@@ -133,7 +133,17 @@ const ExercisesList = (props) => {
                           animate={{ height: hover === row.id ? "auto" : 0 }}
                           className="overflow-hidden"
                         >
-                          <Typography variant="caption" color="text.secondary">
+                          <Typography
+                            variant="caption"
+                            color="text.secondary"
+                            sx={{
+                              display: "-webkit-box",
+                              WebkitLineClamp: 3,
+                              WebkitBoxOrient: "vertical",
+                              overflow: "hidden",
+                              textOverflow: "ellipsis",
+                            }}
+                          >
                             {row.description}
                           </Typography>
                         </motion.div>
